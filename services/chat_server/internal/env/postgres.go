59688935daecb0651c9c769e3c12cd7fa4c7e4bc
Evgenia Kivotova <e.kivotova@gmail.com>
2024-07-21 13:35:44 +0600

    Deploy chat-server and add postgres

---
+++ services/chat_server/internal/env/postgres.go
@@ -0,0 +1,68 @@
+package env
+
+import (
+	"fmt"
+	"os"
+
+	config "github.com/Genvekt/cli-chat/services/chat-server/internal"
+)
+
+var _ config.PostgresConfig = (*postgresConfigEnv)(nil)
+
+const (
+	dbNameEnv     = "PG_DATABASE_NAME"
+	dbUserEnv     = "PG_USER"
+	dbPasswordEnv = "PG_PASSWORD"
+	dbHostEnv     = "PG_HOST"
+	dbPostEnv     = "PG_PORT"
+	dbDSNTemplate = "host=%s port=%s dbname=%s user=%s password=%s sslmode=disable"
+)
+
+type postgresConfigEnv struct {
+	dbName     string
+	dbUser     string
+	dbPassword string
+	dbHost     string
+	dbPort     string
+}
+
+// NewPostgresConfigEnv retrieves new postgresConfigEnv instance
+func NewPostgresConfigEnv() (*postgresConfigEnv, error) {
+	dbName := os.Getenv(dbNameEnv)
+	if dbName == "" {
+		return nil, fmt.Errorf("environment variable %s not set", dbNameEnv)
+	}
+
+	dbUser := os.Getenv(dbUserEnv)
+	if dbUser == "" {
+		return nil, fmt.Errorf("environment variable %s not set", dbUserEnv)
+	}
+
+	dbPassword := os.Getenv(dbPasswordEnv)
+	if dbPassword == "" {
+		return nil, fmt.Errorf("environment variable %s not set", dbPasswordEnv)
+	}
+
+	dbHost := os.Getenv(dbHostEnv)
+	if dbHost == "" {
+		return nil, fmt.Errorf("environment variable %s not set", dbHostEnv)
+	}
+
+	dbPort := os.Getenv(dbPostEnv)
+	if dbPort == "" {
+		return nil, fmt.Errorf("environment variable %s not set", dbPostEnv)
+	}
+
+	return &postgresConfigEnv{
+		dbName:     dbName,
+		dbUser:     dbUser,
+		dbPassword: dbPassword,
+		dbHost:     dbHost,
+		dbPort:     dbPort,
+	}, nil
+}
+
+// DSN returns postgres connection string
+func (p *postgresConfigEnv) DSN() string {
+	return fmt.Sprintf(dbDSNTemplate, p.dbHost, p.dbPort, p.dbName, p.dbUser, p.dbPassword)
+}
